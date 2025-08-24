a8794421e157604c7259f10bf59a9a7565a3dacc
Ben Simkin <ben@businessnet.com.au>
2025-08-25 09:40:49 +1000

    Add admin settings system with multiple clients per project control

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -8,8 +8,61 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     use HasFactory;
+
     protected $fillable = [
-        'variable',
-        'value'
+        'key',
+        'value',
+        'type',
+        'group',
+        'description'
     ];
+
+    /**
+     * Get a setting value by key
+     */
+    public static function getValue($key, $default = null)
+    {
+        $setting = self::where('key', $key)->first();
+        
+        if (!$setting) {
+            return $default;
+        }
+
+        switch ($setting->type) {
+            case 'boolean':
+                return (bool) $setting->value;
+            case 'integer':
+                return (int) $setting->value;
+            case 'json':
+                return json_decode($setting->value, true);
+            default:
+                return $setting->value;
+        }
+    }
+
+    /**
+     * Set a setting value
+     */
+    public static function setValue($key, $value, $type = 'string', $group = 'general', $description = null)
+    {
+        $setting = self::updateOrCreate(
+            ['key' => $key],
+            [
+                'value' => is_array($value) ? json_encode($value) : (string) $value,
+                'type' => $type,
+                'group' => $group,
+                'description' => $description
+            ]
+        );
+
+        return $setting;
+    }
+
+    /**
+     * Check if a setting is enabled (for boolean settings)
+     */
+    public static function isEnabled($key, $default = false)
+    {
+        return self::getValue($key, $default);
+    }
 }
---
+++ database/migrations/2025_08_24_150100_seed_default_settings.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Insert default settings
+        $settings = [
+            [
+                'key' => 'allow_multiple_clients_per_project',
+                'value' => '0', // Default to false
+                'type' => 'boolean',
+                'group' => 'project',
+                'description' => 'Allow multiple clients to be assigned to a single project'
+            ],
+            [
+                'key' => 'require_project_client',
+                'value' => '1', // Default to true
+                'type' => 'boolean',
+                'group' => 'project',
+                'description' => 'Require at least one client to be assigned to a project'
+            ],
+            [
+                'key' => 'max_clients_per_project',
+                'value' => '5', // Default to 5
+                'type' => 'integer',
+                'group' => 'project',
+                'description' => 'Maximum number of clients that can be assigned to a single project'
+            ]
+        ];
+
+        foreach ($settings as $setting) {
+            DB::table('settings')->insert($setting);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('settings')->whereIn('key', [
+            'allow_multiple_clients_per_project',
+            'require_project_client',
+            'max_clients_per_project'
+        ])->delete();
+    }
+};
