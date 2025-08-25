282cbab47c0363db78d9da4bc542c8f79d0ddc3b
Ben Simkin <ben@businessnet.com.au>
2025-08-25 21:31:23 +1000

    Remove debug logging - API is working correctly

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -62,24 +62,6 @@ class DashboardController extends BaseController
                 ->limit(10)
                 ->get();
 
-            // Temporary debug: Log the structure of the first task
-            if ($recentTasks->count() > 0) {
-                $firstTask = $recentTasks->first();
-                \Log::info('Task debug:', [
-                    'id' => $firstTask->id,
-                    'title' => $firstTask->title,
-                    'status_id' => $firstTask->status_id,
-                    'status_loaded' => $firstTask->relationLoaded('status'),
-                    'status_data' => $firstTask->status ? [
-                        'id' => $firstTask->status->id,
-                        'title' => $firstTask->status->title,
-                        'name' => $firstTask->status->name,
-                        'to_array' => $firstTask->status->toArray()
-                    ] : null,
-                    'task_to_array' => $firstTask->toArray()
-                ]);
-            }
-
             // Get user activity
             $userActivity = $this->getUserActivity();
 
