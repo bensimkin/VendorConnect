10d6658c572d17767051e7db80c264b535d8899c
bensimkin <ben@businessnet.com.au>
2025-08-28 16:21:20 +1000

    Add comprehensive debug logging for client detail page and API endpoints

---
+++ public/_next/static/chunks/ac79c8dcd801a3d1.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/798009a8c2f738a1.js",
+  "static/chunks/turbopack-857c23a5fb642016.js"
+])
---
+++ public/_next/static/chunks/c8236f94dc68c6c4.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/798009a8c2f738a1.js",
+  "static/chunks/turbopack-f1a1dd81a068d55d.js"
+])
